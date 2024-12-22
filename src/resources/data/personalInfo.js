193894038d50f209e1ac467e3a7e51989784f024
Christopher Pinedo <christopher.pinedo@hotmail.com>
2024-12-22 13:33:39 -0500

    Migrando proyecto al nuevo repositorio

--- src/resources/data/personalInfo.js
+++ src/resources/data/personalInfo.js
@@ -14,18 +14,18 @@ const personalInfo = {
     presentationMessage1:
       'Me considero una persona proactiva, resiliente y entusiasta por la tecnología que busca aprender constantemente.',
     presentationMessage2:
-      'En desarrollo estoy orientado a frontend web en el ecosistema de React, además poseo conocimiento en Backend con Node.js y Bases de datos SQL y NoSQL.',
+      'Como desarrollador de software estoy orientado a frontend web en el ecosistema de React.js con conocimiento en Backend con Node.js y Bases de datos SQL y NoSQL.',
     presentationMessage3:
-      'En gestión de proyectos me he desempeñado en el area industrial a nivel de procesos administrativos y en el area de TI en sistemas de información y desarrollo de software.',
+      'En gestión de proyectos me he desempeñado en el area industrial a nivel de procesos administrativos y en la gestión de sistemas de información.',
     telephone: {
       number: '978544045',
       countryCode: '+51'
     },
-    email: 'christopher.pinedo@hotmail.com',
+    email: 'cdpg.dev@gmail.com',
     socialMedia: {
-      linkedin: 'linkedin.com/in/christopherpinedo/',
-      github: 'github.com/ChristopherPinedo/',
-      personalWeb: 'christopherpinedo.github.io/web/',
+      linkedin: 'linkedin.com/in/christopher-pinedo-gutierrez/',
+      github: 'github.com/christopherpinedogutierrez/',
+      personalWeb: 'christopherpinedogutierrez.github.io/web/',
       codewars: 'codewars.com/users/ChristopherPinedo/',
       leetCode: 'leetcode.com/ChristopherPinedo/'
     }
